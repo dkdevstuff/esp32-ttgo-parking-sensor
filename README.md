f5045f25bed2f7b80209720e920a250ae4ca9634
dkdevstuff <dkdevstuff@dkdevstuff.com>
2022-12-16 15:17:52 +0200

    added images and some cleanup

--- README.md
+++ README.md
@@ -2,17 +2,36 @@
 
 Shows when to stop when parking in your garage using a distance sensor and the TTGO display.
 
+![demo](demo.gif)
+
 ## Hardware
 
 - TTGO T-Display ESP32 1.14 Inch Wifi Module
-- HC-SR04 ultrasonic sensor
+- HC-SR04 ultrasonic sensor (distance)
 
 ## Setup
 
+- Arduino project dir is `src/parking`
+- Install TFT_eSPI Arduino lib
+- Edit `Arduino/libraries/TFT_eSPI/User_Setup_Select.h`
+
+```c
+/* Comment out line ~30 */
+//#include <User_Setup.h>
+
+/* Uncomment line ~61 */
+#include <User_Setups/Setup25_TTGO_T_Display.h>
+```
+
+- Update some settings in `parking.ino`
+- Upload sketch.
+
 ## Helpful links
+
+- TFT_eSPI https://github.com/Bodmer/TFT_eSPI
+- HC-SR04 setup https://randomnerdtutorials.com/esp32-hc-sr04-ultrasonic-arduino
+- Images generated with http://www.rinkydinkelectronics.com/t_imageconverter565.php
 
 ## TODO
 
-- Images
-- Cleanup
-- Preview gif
+- Vehicle presence MQTT/HTTP for HomeAssistant.
--- src/parking/parking.ino
+++ src/parking/parking.ino
@@ -1,24 +1,21 @@
 #include <SPI.h>
-#include <TFT_eSPI.h>       // Include the graphics library - install and configure https://github.com/Bodmer/TFT_eSPI
+#include <TFT_eSPI.h> // Include the graphics library - install and configure https://github.com/Bodmer/TFT_eSPI
 #include "NotoSansBold15.h"
-#include "NotoSansBold36.h"
-
-// The font names are arrays references, thus must NOT be in quotes ""
-#define AA_FONT_SMALL NotoSansBold15
-#define AA_FONT_LARGE NotoSansBold36
+#include "ImgGo.h"
+#include "ImgSlow.h"
+#include "ImgStop.h"
+#include "ImgBack.h"
 
 // Settings
 #define SOUND_SPEED 0.034 // sound speed in cm/uS
-#define SCREEN_W 135
-#define SCREEN_H 240
 #define GPIO_TRIG 32
 #define GPIO_ECHO 33
 #define GPIO_BL 4
 #define SLOW_DOWN 97
 #define STOP_FROM 77 // inclusve
-#define STOP_TO 73 // inclusve
+#define STOP_TO 73   // inclusve
 
-TFT_eSPI tft = TFT_eSPI();  // Create object "tft"
+TFT_eSPI tft = TFT_eSPI(); // Create object "tft"
 
 long duration;
 float distanceCm = 150;
@@ -27,13 +24,15 @@ bool triggerChange = false;
 bool backlight = true;
 int sleepCtr = 0;
 int testCtr = 0;
-int chevronOffset = 0;
+int moveState = 1;
 
-void setup() {
+void setup()
+{
   tft.init();
   tft.setRotation(2);
+  tft.setSwapBytes(true);
   tft.fillScreen(TFT_BLACK);
-  tft.loadFont(AA_FONT_LARGE);
+  tft.loadFont(NotoSansBold15);
   pinMode(GPIO_TRIG, OUTPUT);
   pinMode(GPIO_ECHO, INPUT);
 }
@@ -41,11 +40,15 @@ void setup() {
 void loop()
 {
   checkDistance();
-  //mockCheckDistance();
+  // mockCheckDistance();
+  distanceChanges();
 
-  if (sleepCtr > 250) {
+  if (sleepCtr > 250)
+  {
     backlightOff();
-  } else {
+  }
+  else
+  {
     if (triggerChange)
     {
       backlightOn();
@@ -53,7 +56,8 @@ void loop()
     }
   }
 
-  if (sleepCtr > 99999) {
+  if (sleepCtr > 99999)
+  {
     sleepCtr = 999;
   }
   sleepCtr++;
@@ -61,8 +65,10 @@ void loop()
   delay(40);
 }
 
-void mockCheckDistance() {
-  if (testCtr == 50) {
+void mockCheckDistance()
+{
+  if (testCtr == 50)
+  {
     distanceCm = SLOW_DOWN;
   }
   else if (testCtr == 100)
@@ -73,80 +79,61 @@ void mockCheckDistance() {
   {
     distanceCm = STOP_TO - 5;
   }
-  else if (testCtr == 400)
+  else if (testCtr == 600)
   {
     distanceCm = 150;
     testCtr = 0;
   }
   testCtr++;
-  distanceChanges();
 }
 
 void checkDistance()
 {
-  digitalWrite(GPIO_TRIG, LOW); // Clears the GPIO_TRIG
+  digitalWrite(GPIO_TRIG, LOW);
   delayMicroseconds(2);
-  digitalWrite(GPIO_TRIG, HIGH); // Sets the GPIO_TRIG on HIGH state for 10 micro seconds
+  digitalWrite(GPIO_TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(GPIO_TRIG, LOW);
-  duration = pulseIn(GPIO_ECHO, HIGH); // Reads the GPIO_ECHO, returns the sound wave travel time in microseconds
+  duration = pulseIn(GPIO_ECHO, HIGH);     // Reads the GPIO_ECHO, returns the sound wave travel time in microseconds
   distanceCm = duration * SOUND_SPEED / 2; // Calculate distance
-  distanceChanges();
 }
 
-void distanceChanges() {
+void distanceChanges()
+{
   triggerChange = abs(previousDistanceCm - distanceCm) >= 0.5;
   previousDistanceCm = distanceCm;
-  if (triggerChange) {
+  if (triggerChange)
+  {
     sleepCtr = 0;
   }
 }
 
 void updateScreen()
-{  
+{
   if (distanceCm > SLOW_DOWN)
   {
-    tft.fillScreen(TFT_GREEN);
-    tft.setTextColor(TFT_BLACK, TFT_GREEN, true);
+    tft.pushImage(0, 0, 135, 240, ImgGo);
   }
   else if (distanceCm <= SLOW_DOWN && distanceCm > STOP_FROM)
   {
-    tft.fillScreen(TFT_ORANGE);
-    tft.setTextColor(TFT_BLACK, TFT_ORANGE, true);
+    tft.pushImage(0, 0, 135, 240, ImgSlow);
   }
   else if (distanceCm <= STOP_FROM && distanceCm >= STOP_TO)
   {
-    tft.fillScreen(TFT_RED);
-    tft.setTextColor(TFT_BLACK, TFT_RED, true);
+    tft.pushImage(0, 0, 135, 240, ImgStop);
   }
   else
   {
-    tft.fillScreen(TFT_PURPLE);
-    tft.setTextColor(TFT_WHITE, TFT_PURPLE, true);
+    tft.pushImage(0, 0, 135, 240, ImgBack);
   }
-
-  tft.setCursor(10, 50);
+  tft.setCursor(55, 220);
   tft.print((int)distanceCm);
-
-  //tft.fillScreen(TFT_BLACK);
-  //tft.pushImage(0, 0 - chevronOffset, 135, 59 - chevronOffset, ChevronGreen);
-  //tft.pushImage(0, 61 - chevronOffset, 135, 121 - chevronOffset, ChevronGreen02);
-  //tft.pushImage(0, 119 - chevronOffset, 135, 179 - chevronOffset, ChevronGreen);
-  //tft.pushImage(0, 240 - chevronOffset, 135, 300 - chevronOffset, ChevronGreen);
-  // chevronOffset++;
-  // if (chevronOffset > 60) chevronOffset = 0;
-
-  // tft.fillScreen(TFT_BLACK);
-  // tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
-  // tft.loadFont(AA_FONT_LARGE);
-  // tft.setCursor(10, 50);
-  // tft.print(distanceCm);
-
 }
 
 void backlightOn()
 {
-  if (!backlight) {
+  if (!backlight)
+  {
     digitalWrite(GPIO_BL, HIGH);
     backlight = true;
   }
@@ -154,51 +141,10 @@ void backlightOn()
 
 void backlightOff()
 {
-  if (backlight) {
+  if (backlight)
+  {
+    tft.fillScreen(TFT_BLACK);
     digitalWrite(GPIO_BL, LOW);
     backlight = false;
   }
-}
-
-void drawArrow()
-{
-  tft.fillScreen(TFT_BLACK);
-  int xMid = SCREEN_W / 2;
-  for (int y = 0; y < SCREEN_H; y++)
-  {
-    if (y < xMid)
-    {
-      tft.drawLine(xMid - y, y, xMid + y, y, TFT_GREEN);
-    }
-    else
-    {
-      tft.drawLine(0, y, SCREEN_W - 1, y, TFT_GREEN);
-    }
-  }
-}
-
-void testScreenSize()
-{
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
-  tft.loadFont(AA_FONT_LARGE);
-
-  int x = 0;
-  int y = 0;
-
-  while (x < SCREEN_W || y < SCREEN_H) {
-    if (x < SCREEN_W) {
-      tft.setCursor(30, 50);
-      tft.drawPixel(x, 0, TFT_WHITE);
-      tft.print(++x);
-    }
-    if (y < SCREEN_H) {
-      tft.setCursor(30, 150);
-      tft.drawPixel(0, y, TFT_WHITE);
-      tft.print(++y);
-    }
-    delay(10);
-  }
-  tft.unloadFont();
-  delay(5000);
 }
